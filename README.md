a0875f5fa880c4c8c70f71d20464ae1a9901cc74
EzequiasEyaire <esantos@xalode.com>
2025-01-10 22:08:27 -0400

    only client

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ python -m pip install --upgrade pip
 #pip install grpcio 
 #pip install grpcio-tools
 
+update 
+python -m grpc_tools.protoc -Iprotos --python_out=. --pyi_out=. --grpc_python_out=. protos/helloworld.proto
+
+
+-- M
+
+# 1 Init config (Database download)
+# 2 Live Event
+# 3 Activate Relay
+# 4 Update Config
 
 
 
--- greeter_client.py
+++ greeter_client.py
@@ -31,6 +31,7 @@ def run():
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         
         response = stub.SayHello(helloworld_pb2.HelloRequest(name="you"))
+        
         print("Greeter client received: " + response.message)
         
         response = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
